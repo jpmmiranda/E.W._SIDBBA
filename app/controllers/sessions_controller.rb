16d676d3bf1ec9fc26fb9c59eafa4ca4ec48ade8
gtox19 <rfbm1319@gmail.com>
2017-05-25 17:12:54 +0100

    depois o miranda faz

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,12 @@
 class SessionsController < ApplicationController
-	def index
-	end
-	def new
-	end
+	
 
 	def create
 			user = User.find_by(email: params[:session][:email].downcase)
 			if user && user.authenticate(params[:session][:password])
 			# Log the user in and redirect to the user's show page.
+			redirect_to '/login_home'
+
 			else
 				# Create an error message.
 				render 'new'
