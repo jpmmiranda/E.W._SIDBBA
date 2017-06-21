2e763c33bd09a5811c4ea36927968b2b1fc48930
João Monteiro <monteiro_vsc@hotmail.com>
2017-06-21 17:55:59 +0100

    alertas

--- app/controllers/inicial_controller.rb
+++ app/controllers/inicial_controller.rb
@@ -2,6 +2,50 @@ class InicialController < ApplicationController
   respond_to :js, :html
 
 	def index
+		count=0
+		user_id1 = current_user.id
+		
+		@leituras1=Registo.group('"local_id"')
+		@leituras = Registo.group('"local_id"').having('data = MAX(data)')
+		
+		@leituras1.each do |leitura|
+			Alerta.all.each do |alerta|
+				if alerta.user_id==user_id1 && leitura.local_id==alerta.sensor_id
+					if leitura.temperatura <= alerta.max_temp && leitura.temperatura >= alerta.min_temp
+						if count==0
+							flash[:success] = "Alerta Temperatura No Sensor #{leitura.local_id} --- "
+							count=1
+						else
+							flash[:success] << "Alerta Temperatura No Sensor #{leitura.local_id} --- "
+						end
+					end
+					if leitura.luminosidade <= alerta.max_luminosidade && leitura.luminosidade >= alerta.min_luminosidade
+						if count==0
+							flash[:success] = "alerta luminosidade No Sensor #{leitura.local_id} --- "
+							count=1
+						else
+							flash[:success] << "alerta luminosidade No Sensor #{leitura.local_id} --- "
+						end
+					end
+					if leitura.humidade <= alerta.max_humidade && leitura.humidade >= alerta.min_humidade
+						if count==0
+							flash[:success] = "alerta humidade No Sensor #{leitura.local_id} --- "
+							count=1
+						else
+							flash[:success] << "alerta humidade No Sensor #{leitura.local_id} --- "
+						end
+					end
+					if leitura.pressao <= alerta.max_pressao && leitura.pressao >= alerta.min_pressao
+						if count==0
+							flash[:success] = "alerta pressao No Sensor #{leitura.local_id} --- "
+							count=1
+						else
+							flash[:success] << "alerta pressao No Sensor #{leitura.local_id} --- "
+						end
+					end
+				end
+			end
+		end
 	  	 @hash = Gmaps4rails.build_markers(Locai.all) do |record, marker|
 	  	 getValores(record.id)
 	     marker.lat record.local_latitude
